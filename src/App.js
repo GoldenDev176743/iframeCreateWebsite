0b6bb7b895f77bfbcc4708ec4061c36a6677a068
Erwin Hofmann <erwinhofmann647@gmail.com>
2023-11-22 10:28:56 +0900

    redux-store'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-5xl font-bold underline">Hello world!</h1>
-      </header>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
